bf630d345e07b1ef9a9ee3f44f7b02282c81d4dd
Gathan Naufarrel Akhtarachman <gathanakhtar@gmail.com>
2026-04-17 18:28:18 +0800

    BIG COMMIT

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 150
 const JUMP_VELOCITY = -250.0
 
+@onready var anim = $AnimatedSprite2D
+
+# We use this to remember which way we were looking when we stop
+var last_direction = "Right"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,12 +17,31 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction: -1, 0, or 1
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		# Update our tracker whenever there is movement input
+		if direction > 0:
+			last_direction = "Right"
+		else:
+			last_direction = "Left"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Animation Logic
+	if is_on_floor():
+		if velocity.x > 0:
+			anim.play("Run Right")
+		elif velocity.x < 0:
+			anim.play("Run Left")
+		else:
+			# If stopped, play the Idle animation for the last direction moved
+			# This assumes you have "Idle Right" and "Idle Left" animations
+			anim.play("Idle " + last_direction)
+	else:
+		# Optional: Add a jump animation here if you have one!
+		pass
 
 	move_and_slide()
